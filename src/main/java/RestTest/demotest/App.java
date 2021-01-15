213910b8d4b4dc8683ec0100afa2ca31db1b79a6
Pranay Raj <pranayrajnagella@gmail.com>
2021-01-15 19:51:24 +0530

    First Commit Api Test

---
+++ src/main/java/RestTest/demotest/App.java
@@ -0,0 +1,13 @@
+package RestTest.demotest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/RestTest/demotest/TestcatApi.java
@@ -0,0 +1,34 @@
+package RestTest.demotest;
+
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.*;
+
+import java.util.HashMap;
+
+public class TestcatApi {
+	
+	@Test()
+	public void testApi()
+	{
+		RestAssured.baseURI="https://cat-fact.herokuapp.com";
+		
+		HashMap<String, Object> QueryMap=new HashMap<String,Object>();
+		QueryMap.put("animal_type","cat");
+		QueryMap.put("amount",11);
+		
+		HashMap<String, String> headerMap=new HashMap<String, String>();
+		headerMap.put("Accept","application/json");
+		headerMap.put("content-type","application/json");
+		
+		Response response=given().log().all().queryParams(QueryMap).headers(headerMap).when().get("/facts")
+				.then().log().all().assertThat().statusCode(200).extract().response();
+		String resp=response.asString();
+		
+		System.out.println(resp);
+	}
+
+}
